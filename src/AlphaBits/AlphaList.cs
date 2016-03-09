8135e403fedccc8aed74abb8c256b13b746eba6a
C-Stein <caitlin.teter@gmail.com>
2016-03-09 13:18:28 -0600

    AlphaBits Exercise Complete

---
+++ src/AlphaBits/AlphaList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlphaBits
+{
+    public class AlphaList
+    {
+        public List<char> userList = new List<char>();
+
+        public AlphaList()
+        {
+            List<char> userList = new List<char>();
+        }
+
+        public void addChar(char charToAdd)
+        {
+            charToAdd = Char.ToLower(charToAdd);
+            string thingToAdd = charToAdd.ToString(); 
+
+            if (userList.Contains(charToAdd))
+            {
+                Console.WriteLine("Sorry! You've already added {0}", thingToAdd);
+                return;
+            }
+            if (!Char.IsLetter(charToAdd))
+            {
+                Console.WriteLine("Sorry! '{0}' is not a letter", thingToAdd);
+                return;
+            }
+            userList.Add(charToAdd);
+            //if list already contains char, or char is not a letter throw an error
+        }
+
+        public int listLength()
+        {
+            return userList.Count();
+        }
+
+        public string returnList()
+        {
+            string combindedString = string.Join(",", userList.ToArray());
+            return combindedString;
+        }
+    }
+}
+;
---
+++ src/AlphaBits/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlphaBits
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            int correctCount = 0;
+
+            Console.Write("Welcome to AlphaBits! The goall of this admittedly lame game  is to type all of the letters of the alphabet (in any order)  without hitting any non-letter characters \n");
+
+            //type "hint" for a hint;
+            AlphaList userList = new AlphaList();
+
+            while (correctCount != 26)
+            {
+                
+
+                Console.Write("You have currently typed {0} members of the alphabet. Type one!\n", correctCount);
+
+                string input = Console.ReadLine();
+
+                if(input.Length != 1)
+                {
+                    Console.Write("Please enter exactly one letter. If you enter more than one letter, only the first letter will count. \n");
+                }
+
+                if(input == "!")
+                {
+
+                    String charString = userList.returnList();
+                    Console.Write("Current userList is {0} \n", charString);
+                }
+
+                char inputChar = input[0];
+
+                userList.addChar(inputChar);
+
+                correctCount = userList.listLength();
+
+            }
+
+            Console.WriteLine("Congratulations!You typed all the letters!");
+            Console.ReadLine();
+        }
+    }
+}
